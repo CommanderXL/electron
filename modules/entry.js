a33ba5c6013fac34e48f0d37092c487703d173c5
XRene <1017653702@qq.com>
2016-05-26 11:03:23 +0800

    添加angular-material所有的配置文件,其中svg_icon是从第三方获取

--- modules/entry.js
+++ modules/entry.js
@@ -1,7 +1,7 @@
 /**
  * Created by XRene on 16/4/2.
  */
-
+require('mainCss/angular-material.min.css');
 require('mainCss/demo.less');
 
 require('./main');
--- modules/main.js
+++ modules/main.js
@@ -7,13 +7,22 @@
 const uiRouter = require('angular-ui-router');*/
 
 import angular from 'angular';
+import ngMessages from 'angular-messages';
+import ngAria from 'angular-animate';
+import ngAnimate from 'angular-animate';
+import ngMaterial from 'angular-material';
+var materialSVG = require('./lib/material_svg');
 var uiRouter = require('angular-ui-router');
 
-module.exports = angular.module('myApp', ['ui.router'])
-    .controller('demoCtrl', ['$scope', function($scope) {
-        "use strict";
-        $scope.data = 'Hello wolrd';
-    }]);
+module.exports = angular.module('myApp', ['ui.router', 'ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+    .controller('AppCtrl', function($scope) {
+        $scope.title1 = 'Button';
+        $scope.title4 = 'Warn';
+        $scope.isDisabled = true;
+
+        $scope.googleUrl = 'http://google.com';
+
+    });
 
 
 angular.bootstrap(document, ['myApp']);
